f36e92da55db410dd89079838e044f35a4aece66
UltimateOgee <Shubhankar.singh@outlook.com>
2016-05-09 08:44:46 -0700

    Update PixelEater.java

--- PixelEater/src/pixeleater/PixelEater.java
+++ PixelEater/src/pixeleater/PixelEater.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package pixeleater;
 
-/**
- *
- * @author singshu17
- */
+
 public class PixelEater {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
