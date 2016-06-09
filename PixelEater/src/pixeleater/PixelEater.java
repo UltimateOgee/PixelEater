d3ddf59ffc6fec08dd73e0a0891aad5bfeed80d6
Surryy <n.nguyen364@hotmail.com>
2016-06-09 09:30:00 -0700

    Update PixelEater.java

--- PixelEater/src/pixeleater/PixelEater.java
+++ PixelEater/src/pixeleater/PixelEater.java
@@ -1,7 +1,4 @@
-/*
- * Shubhankar Singh 2016
- */
-package pixeleater;
+package lwjgl.program;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -18,13 +15,18 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.Sys;
 
-public class PixelEater {
-    double x = 27.5;
+public class LWJGLProgram {
+
+    int score = 0;
+    double x = 23.5;
     double y = 46.5;
+    int[][] gameboard = new int[61][55];
+    int height = 61;
+    int width = 55;
 
     public void start() throws IOException {
         try {
-            Display.setDisplayMode(new DisplayMode(1110, 1220)); //added ten to x and y to prevent out of bound error
+            Display.setDisplayMode(new DisplayMode(550, 610)); //added ten to x and y to prevent out of bound error
             Display.create();
         } catch (LWJGLException e) {
             e.printStackTrace();
@@ -42,11 +44,12 @@ public class PixelEater {
 
             // set the color of the quad (R,G,B,A)
             GL11.glColor3f(0f, 38f, 255f);
-
+            //put the file into memory and then modify the memory
             // draw quad
             File file = new File("Pacman Board.png");
             GL11.glBegin(GL11.GL_QUADS);
             getColorValue(file);
+            drawboard();
             pacman();
             updatePos(file);
             GL11.glEnd();
@@ -57,79 +60,132 @@ public class PixelEater {
         Display.destroy();
     }
 
-    public static void getColorValue(File file) throws IOException {
+    public void getColorValue(File file) throws IOException {
 
         BufferedImage image = ImageIO.read(file);
         int width = image.getWidth();
         int height = image.getHeight();
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
 
-                int clr = image.getRGB(i, j);
-                int red = (clr & 0x00ff0000) >> 16;
-                int green = (clr & 0x0000ff00) >> 8;
-                int blue = clr & 0x000000ff;
-
-                GL11.glColor3f(red, green, blue);
-                GL11.glVertex2f(i, j);
-                    GL11.glVertex2f(i + 1, j);
-                    GL11.glVertex2f(i + 1, j + 1);
-                    GL11.glVertex2f(i, j + 1);
+                int clr = image.getRGB(j, i);
+                gameboard[height - i-1][j] = clr;
                 
+                //int red = (clr & 0x00ff0000) >> 16;
+                //int green = (clr & 0x0000ff00) >> 8;
+                //int blue = clr & 0x000000ff;
+
+                //GL11.glColor3f(red, green, blue);
+                //GL11.glVertex2f(i, j);
+                //    GL11.glVertex2f(i + 1, j);
+                //    GL11.glVertex2f(i + 1, j + 1);
+                //    GL11.glVertex2f(i, j + 1);
             }
         }
 
     }
-    public  void updatePos(File file) throws IOException{
+
+    public void drawboard() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int red = (gameboard[j][i] & 0x00ff0000) >> 16;
+                int green = (gameboard[j][i] & 0x0000ff00) >> 8;
+                int blue = gameboard[j][i] & 0x000000ff;
+                GL11.glColor3f(red, green, blue);
+                GL11.glVertex2f(i,height - j);
+                GL11.glVertex2f((i + 1), height -j);
+                GL11.glVertex2f((i + 1),height - j+1);
+                GL11.glVertex2f(i, height -j+1 );
+            }
+        }
+    }
+
+    public void updatePos(File file) throws IOException {
         BufferedImage image = ImageIO.read(file);
+        float border = 1.49999f;
+        int yPlaceholder = (int) y;
+        int xPlaceholder = (int) x;
+        int colorRight = gameboard[height - yPlaceholder][xPlaceholder+1];
+        int colorLeft = gameboard[height - yPlaceholder][xPlaceholder-1];
+        int colorTop = gameboard[(height - yPlaceholder) +1][xPlaceholder];
+        int colorBottom = gameboard[(height - yPlaceholder) -1][xPlaceholder];
+        System.out.println("coordinates: "+ (height - yPlaceholder) + ","+ xPlaceholder +"Right: "+colorRight + "Left: " +colorLeft + "Top: "+colorTop + "Bottom: " +colorBottom);
+
         
-        int colortr = image.getRGB((int)x + 1, (int)y + 1);
-        int colortl = image.getRGB((int)x - 1, (int)y + 1);
-        int colorbr = image.getRGB((int)x + 1, (int)y - 1);
-        int colorbl = image.getRGB((int)x - 1, (int)y - 1);
-        
-        System.out.println("Color: " + colortr + " Coordinates: " + x + ", " + y);
-        
-        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) x -= .01;
-	if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) x +=.01;
- 
-	if (Keyboard.isKeyDown(Keyboard.KEY_UP)) y -= .01;
-	if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) y += .01;
-        
-        if((colortr == -16767233)){x-=.01;y-=.01;}
-        if((colortl == -16767233)){x+=.01;y-=.01;}
-        if((colorbr == -16767233)){x-=.01;y+=.01;}
-        if((colorbl == -16767233)){x+=.01;y+=.01;}
-        
-        if (x < 0) x = 0;
-        if (x > 550) x = 550;
-	if (y < 0) y = 0;
-	if (y > 610) y = 610;
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT) && colorLeft != -16767233) {
+            x -= .01;
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT) && colorRight != -16767233) {
+            x += .01;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP)&&colorTop != -16767233) {
+            y -= .01;
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)&& colorBottom != -16767233) {
+            y += .01;
+        }
+
+       /* if ((colorRight == -16767233)) {
+            x -= .01;
+        }
+        if ((colorLeft == -16767233)) {
+            x += .01;
+        }
+        if ((colorBottom == -16767233)) {
+            y -= .01;
+        }
+        if ((colorTop == -16767233)) {
+            y += .01;
+        } */
+
+        if (colorRight == -10240 || colorLeft == -10240 || colorBottom == -10240 || colorTop == -10240) {
+            score += 1;
+        }
+
+        if (x < 0) {
+            x = 0;
+        }
+        if (x > 550) {
+            x = 550;
+        }
+        if (y < 0) {
+            y = 0;
+        }
+        if (y > 610) {
+            y = 610;
+        }
+        if (x < 1.0999999999985015) {
+            x = 53.489999999996426;
+        }
+        if (x > 53.489999999996426) {
+            x = 1.1999999999985015;
+        }
     }
-    public void pacman(){
+
+    public void pacman() {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
- 
-		// R,G,B,A Set The Color To Blue One Time Only
-		GL11.glColor3f(0f, 255f, 0f);
- 
-		// draw quad
-		GL11.glPushMatrix();
-			GL11.glTranslatef((float)x, (float)y, 0);
-			GL11.glTranslatef((float)-x, (float)-y, 0);
- 
-			GL11.glBegin(GL11.GL_QUADS);
-				GL11.glVertex2f((float)x - 1, (float)y - 1);
-				GL11.glVertex2f((float)x + 1, (float)y - 1);
-				GL11.glVertex2f((float)x + 1, (float)y + 1);
-				GL11.glVertex2f((float)x - 1, (float)y + 1);
-			GL11.glEnd();
-		GL11.glPopMatrix();
+
+        // R,G,B,A Set The Color To Blue One Time Only
+        GL11.glColor3f(0f, 255f, 0f);
+
+        // draw quad
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float) x, (float) y, 0);
+        GL11.glTranslatef((float) -x, (float) -y, 0);
+
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f((float) x - 1, (float) y - 1);
+        GL11.glVertex2f((float) x + 1, (float) y - 1);
+        GL11.glVertex2f((float) x + 1, (float) y + 1);
+        GL11.glVertex2f((float) x - 1, (float) y + 1);
+        GL11.glEnd();
+        GL11.glPopMatrix();
     }
-    
 
     public static void main(String[] argv) throws IOException {
-        PixelEater game = new PixelEater();
-        File file = new File("Pacman Board.png");
+        LWJGLProgram game = new LWJGLProgram();
         game.start();
 
     }
