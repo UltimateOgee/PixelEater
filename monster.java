9ebde5e9179e60b9b191292d16d362e8dfb6b8b5
UltimateOgee <Shubhankar.singh@outlook.com>
2016-06-10 04:07:33 -0700

    Update monster.java

--- monster.java
+++ monster.java
@@ -9,6 +9,7 @@ package pixeleater;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 import javax.imageio.ImageIO;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
@@ -28,9 +29,9 @@ public class monster {
     int score = 0;
     double x = 39.5;
     double y = 47.5;
+    double xp, yp;
     
     public monster(){
-        
     }
     
     public void monster() {
@@ -53,6 +54,10 @@ public class monster {
     }
     
     public void updatePos(File file, int[][] gameboard, int height) throws IOException {
+        
+        Random generator = new Random(); 
+        int r = generator.nextInt(4);
+        
         BufferedImage image = ImageIO.read(file);
         float border = 1.49999f;
         int yPlaceholder = (int) y;
@@ -63,7 +68,10 @@ public class monster {
         int colorBottom = gameboard[(height - yPlaceholder) -1][xPlaceholder];
         System.out.println("coordinates: "+ (height - yPlaceholder) + ","+ xPlaceholder +"Right: "+colorRight + "Left: " +colorLeft + "Top: "+colorTop + "Bottom: " +colorBottom);
         
+        if(r == 4 && colorLeft != -16767233){x -= 0.05;};
+        if(r == 3 && colorRight != -16767233){x += 0.05;};
+        if(r == 2 && colorTop != -16767233){y += 0.05;};
+        if(r == 1 && colorBottom != -16767233){y -= 0.05;};
         
-    
     }
 }
